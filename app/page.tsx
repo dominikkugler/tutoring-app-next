77776122edcca6920a71d1f03aa442ceef87bb23
dominikkugler <20456@student.ans-elblag.pl>
2025-01-21 19:36:00 +0100

    Connecting to Supabase project and testing Auth module

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import {createClient} from "@/utils/supabase/server";
 
 export default async function Home() {
+  const supabase = await createClient();
+  const {data: categories} = await supabase.from("categories").select("*");
+
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      <div className="flex flex-col items-center">
+        <p className="text-lg font-bold">Welcome to the Supabase connected App</p>
+        <p>Here's the data from Supabase:</p>
+        <ul className="list-disc list-inside mt-4">
+          {categories?.map((category) => (
+            <li key={category.id} className="text-base text-gray-700">
+              {category.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
